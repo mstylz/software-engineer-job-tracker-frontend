817adad0a1b06c4e78391d39135768539c5a6d5b
mstylz <mstylz1985@gmail.com>
2026-08-12 17:10:00 -0400

    Set up React project foundation and structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Software Engineer Job Tracker</h1>
+        <p>
+          Track applications, interviews, and job search progress.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
