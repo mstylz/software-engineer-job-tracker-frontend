c0bc5478bd6cfea6986d809f299ca4a74ac325b7
mstylz <mstylz1985@gmail.com>
2026-08-16 18:55:49 -0400

    Add about and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
+import About from './components/About/About'
+import Footer from './components/Footer/Footer'
 import './App.css'
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className="app">
       <Header />
       <Main />
+      <About />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">
+        © 2026 Software Engineer Job Tracker
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
